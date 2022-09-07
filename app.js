5492b1b1e87f31f15b911764cb225ada31c40e44
赵海鹏 <zhaohp@bjhzwq.com>
2022-09-07 18:12:53 +0800

    feat:添加测试代码和注释

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+var { buildSchema, graphql } = require('graphql');
  
+// https://graphql.cn/
+// GraphQL 是一个用于 API 的查询语言，是一个使用基于类型系统来执行查询的服务端运行时.
+
+// 定义数据结构
 var schema = buildSchema(`
   type Query {
-    hello: String
+    hello: String,
+    foo: String
   }
 `);
- 
-var root = { hello: () => 'Hello world!' };
- 
+
+// 定义数据源。需要与数据结构中的属性一一对应
+var root = { 
+    hello: () => 'Hello world!',
+    foo:()=>{
+        return 'foo'
+    }
+};
+
+// 查询数据。仅仅是查询示意，一般通过客户端发起请求进行查询
+graphql({
+    schema,
+    source:'{ hello }',
+    rootValue:root
+}).then(res=>{
+    console.log("res:",res)
+})
+
+// express服务
 var app = express();
+// 路由监听 /graphql
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+
+// http://localhost:4000/graphql?query=%23%20Welcome%20to%20GraphiQL%0A%23%0A%23%20GraphiQL%20is%20an%20in-browser%20tool%20for%20writing%2C%20validating%2C%20and%0A%23%20testing%20GraphQL%20queries.%0A%23%0A%23%20Type%20queries%20into%20this%20side%20of%20the%20screen%2C%20and%20you%20will%20see%20intelligent%0A%23%20typeaheads%20aware%20of%20the%20current%20GraphQL%20type%20schema%20and%20live%20syntax%20and%0A%23%20validation%20errors%20highlighted%20within%20the%20text.%0A%23%0A%23%20GraphQL%20queries%20typically%20start%20with%20a%20%22%7B%22%20character.%20Lines%20that%20start%0A%23%20with%20a%20%23%20are%20ignored.%0A%23%0A%23%20An%20example%20GraphQL%20query%20might%20look%20like%3A%0A%23%0A%23%20%20%20%20%20%7B%0A%23%20%20%20%20%20%20%20field(arg%3A%20%22value%22)%20%7B%0A%23%20%20%20%20%20%20%20%20%20subField%0A%23%20%20%20%20%20%20%20%7D%0A%23%20%20%20%20%20%7D%0A%23%0A%23%20Keyboard%20shortcuts%3A%0A%23%0A%23%20%20Prettify%20Query%3A%20%20Shift-Ctrl-P%20(or%20press%20the%20prettify%20button%20above)%0A%23%0A%23%20%20%20%20%20Merge%20Query%3A%20%20Shift-Ctrl-M%20(or%20press%20the%20merge%20button%20above)%0A%23%0A%23%20%20%20%20%20%20%20Run%20Query%3A%20%20Ctrl-Enter%20(or%20press%20the%20play%20button%20above)%0A%23%0A%23%20%20%20Auto%20Complete%3A%20%20Ctrl-Space%20(or%20just%20start%20typing)%0A%23%0A%0A%7B%0A%20%20hello%0A%7D
